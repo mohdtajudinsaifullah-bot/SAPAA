455ebc5b64259ee6bc2c62b6a9e408cbf0a303ed
Mohd Tajudin Saifullah Bin Abdul Wahab <m_tajudin@esyariah.gov.my>
2026-09-05 07:05:24 +0800

    Fix UserButton props error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function HomePage() {
             </nav>
 
             <SignedIn>
-              <UserButton afterSignOutUrl="/" />
+              <UserButton />
             </SignedIn>
 
             <SignedOut>
